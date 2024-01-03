11cb1ace065df9ad4de96d521d71663a7fefbd12
hyeonbin <basilla0617@naver.com>
2024-01-03 16:19:35 +0900

    feat : Jdbc 구현 (DI를 이용해 설정 코드만으로 구현 클래스를 변경)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService((MemoryMemberRepository) memberRepository());
@@ -15,6 +26,11 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+
+        // 다른 코드는 건들지 않고 이 부분만 변경.
+        // memory 버전에서 jdbd 버전의 memberRepository로 등록
+        return new JdbcMemberRepository(dataSource);
     }
 }
