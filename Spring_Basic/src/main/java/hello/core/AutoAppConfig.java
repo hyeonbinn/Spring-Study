7cc9b31b6188fc65584379e9441ef87b5605dae3
hyeonbin <basilla0617@naver.com>
2024-02-19 01:33:03 +0900

    feat: 중복 등록과 충돌

--- Spring_Basic/src/main/java/hello/core/AutoAppConfig.java
+++ Spring_Basic/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,4 +17,8 @@ import org.springframework.stereotype.Component;
 )
 
 public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository") // 자동으로 등록된 빈의 이름과 같도록 수동 설정 해봄.
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
