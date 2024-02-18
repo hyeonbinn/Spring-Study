8db708ec47253153b1c6db5c6500fc580aa41abf
hyeonbin <basilla0617@naver.com>
2024-02-19 00:45:28 +0900

    feat: 탐색 위치와 기본 스캔 대상

--- Spring_Basic/src/main/java/hello/core/AutoAppConfig.java
+++ Spring_Basic/src/main/java/hello/core/AutoAppConfig.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         /** 예제 코드를 살리기 위해 쓴 코드. AppConfig에서 @Bean으로 수동으로 등록하고 있기에 충돌나지 않도록 제외하는 코드를 추가한 것. 원래는 없어도 되는 코드 **/
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+
 )
 
 public class AutoAppConfig {
