5127e244f7a37fffeb3057cb0a4fa05e765c520d
hyeonbin <basilla0617@naver.com>
2024-02-18 15:46:09 +0900

    feat: 스프링 빈은 항상 무상태(stateless)로 설계 해야 한다. - 좋은 예시

--- Spring_Basic/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ Spring_Basic/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -16,16 +16,16 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         //ThreadA에서 userA가 10000원을 주문함.
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
 
         //ThreadB에서 userB가 20000원을 주문함.
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         //ThreadA에서 userA의 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price); // 기대 금액 : 10000 / 실제 출력 : 20000
+        //int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice); // 기대 금액 : 10000 / 실제 출력 : 10000
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000); //ture 출력 => 이러면 안됨..!
+        //Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000); //ture 출력 => 이러면 안됨..!
     }
 
     static class TestConfig {
