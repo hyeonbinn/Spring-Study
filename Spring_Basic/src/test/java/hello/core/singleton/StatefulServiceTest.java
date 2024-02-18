540eb41c3da67a7cc0ba850df3e1dbe7d1c841f6
hyeonbin <basilla0617@naver.com>
2024-02-18 15:41:20 +0900

    feat: 스프링 빈은 항상 무상태(stateless)로 설계 해야 한다. - 잘못된 예시

---
+++ Spring_Basic/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,38 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+
+class StatefulServiceTest {
+
+    @Test
+    void statusfulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA에서 userA가 10000원을 주문함.
+        statefulService1.order("userA", 10000);
+
+        //ThreadB에서 userB가 20000원을 주문함.
+        statefulService2.order("userB", 20000);
+
+        //ThreadA에서 userA의 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price); // 기대 금액 : 10000 / 실제 출력 : 20000
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000); //ture 출력 => 이러면 안됨..!
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
