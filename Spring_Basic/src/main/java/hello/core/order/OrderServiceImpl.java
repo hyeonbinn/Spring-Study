80d99646fbed786e227e4e67f8eb7241894edf34
hyeonbin <basilla0617@naver.com>
2024-02-20 14:11:17 +0900

    feat: 생성자 주입

--- Spring_Basic/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring_Basic/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,12 +12,18 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+    /** 생성자 주입 : 불변, 필수 의존 관계에 사용
+     * memberRepository ,discountPolicy는 한 번 생성되면 못 바꿈. **/
+    //@Autowired : 생성자가 1개만 있으면 @Autowired를 생략 가능.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
+    /**아래와 같이 public으로 열어놓는 수정 메서드를 만들어놓는 것은 매우 안 좋음.**/
+//    public void setDiscountPoilcy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
