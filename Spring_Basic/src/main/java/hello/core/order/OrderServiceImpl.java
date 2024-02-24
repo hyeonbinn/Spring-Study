c0b0aed625a9093e782c16c65a39047a2ef7742a
hyeonbin <basilla0617@naver.com>
2024-02-24 23:46:11 +0900

    feat: lombok 설정 및 적용

--- Spring_Basic/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring_Basic/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,29 +4,22 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
+    /** @RequiredArgsConstructor를 사용함으로써 final이 붙은 필드를 모아서 생성자를 자동으로 만들어준다. 즉 아래의 기존 코드와 동일 **/
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//    this.memberRepository = memberRepository;
+//    this.discountPolicy = discountPolicy;
 //    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//    }
-
-        public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
 
     @Override
