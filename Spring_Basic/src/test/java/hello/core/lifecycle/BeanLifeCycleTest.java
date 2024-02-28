f2af37099899b6241f5b3844e6c675970c0cdca5
hyeonbin <basilla0617@naver.com>
2024-02-29 01:44:36 +0900

    feat: @PostConstruct, @PreDestroy 애노테이션을 사용하자

--- Spring_Basic/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ Spring_Basic/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,8 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean(initMethod = "init", destroyMethod = "close") //설정 정보에 초기화, 소멸 메서드 지정
+
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
