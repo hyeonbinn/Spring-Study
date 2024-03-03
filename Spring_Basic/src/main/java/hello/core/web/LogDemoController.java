899ead49dbf5eca92c51a3df0bf2542b8e83b398
hyeonbin <basilla0617@naver.com>
2024-03-04 01:01:29 +0900

    feat: 스코프와 프록시

--- Spring_Basic/src/main/java/hello/core/web/LogDemoController.java
+++ Spring_Basic/src/main/java/hello/core/web/LogDemoController.java
@@ -15,14 +15,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; //myLogger를 주입 받는 게 아닌, 찾을 수 있는 ObjectProvider가 주입 되는 것.
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo") //이런 요청이 오면
     @ResponseBody //뷰 템플렛이 없기에 문자 그대로 응답 보내도록
     public String logDemo(HttpServletRequest request) {
 
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject(); //ObjectProvider의 getObject를 통해 myLogger를 조회
         myLogger.setRequestUrl(requestURL);
 
         myLogger.log("controller test");
