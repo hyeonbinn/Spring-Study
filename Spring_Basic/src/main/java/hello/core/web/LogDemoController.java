b7eca680cb9969ff6db3b0ddc838b6b8ec4b018e
hyeonbin <basilla0617@naver.com>
2024-03-03 17:35:15 +0900

    feat: request 스코프 예제 만들기

---
+++ Spring_Basic/src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo") //이런 요청이 오면
+    @ResponseBody //뷰 템플렛이 없기에 문자 그대로 응답 보내도록
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestUrl(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
